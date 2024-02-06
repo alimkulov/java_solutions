31de25624e2cd78dc0a5e7f8bd1b30eb29cb66eb
arumasu <samlaalmas@mail.ru>
2024-02-06 14:31:46 -0500

    add company

---
+++ com.company/src/com/company/task5.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class task5 {
+
+	 public static void main(String[] args) {
+		 Scanner sc =new Scanner(System.in);
+		 System.out.println("Your string is:"+sc.nextLine());
+	 }
+}
